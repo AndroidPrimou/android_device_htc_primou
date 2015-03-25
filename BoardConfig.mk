793b922b5ab65e7c5462e26ce6e40ff6b9062ddd
jonas2515 <jonas2515@users.noreply.github.com>
2015-03-25 22:43:57 +0100

    BoardConfig: Clean up and update for lollipop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,11 +1,14 @@
 # Inherit common msm7x30 configs
 -include device/htc/msm7x30-common/BoardConfigCommon.mk
 
+# Inherit from the proprietary version
+-include vendor/htc/primou/BoardConfigVendor.mk
+
 
 TARGET_BOOTLOADER_BOARD_NAME := primou
 
 # Kernel
-BOARD_KERNEL_CMDLINE := no_console_suspend=1 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := no_console_suspend=1 androidboot.hardware=htc7x30 androidboot.selinux=permissive
 BOARD_KERNEL_RECOVERY_CMDLINE := $(BOARD_KERNEL_CMDLINE) msmsdcc_power_gpio=88 androidboot.hardware=qcom
 BOARD_KERNEL_BASE := 0x13F00000
 BOARD_KERNEL_PAGE_SIZE := 4096
@@ -15,14 +18,17 @@ BOARD_KERNEL_PAGE_SIZE := 4096
 #mmcblk0p17: 00040000 00000200 "misc"
 #mmcblk0p21: 0087f400 00000200 "recovery"
 #mmcblk0p22: 00400000 00000200 "boot"
-#mmcblk0p25: 22dffe00 00000200 "system"
-#mmcblk0p27: 12bffe00 00000200 "cache"
-#mmcblk0p26: 496ffe00 00000200 "userdata"
-#mmcblk0p28: 014bfe00 00000200 "devlog"
-#mmcblk0p29: 00040000 00000200 "pdata"
+#mmcblk0p25: 31dffe00 00000200 "system"
+#mmcblk0p28: 0afffe00 00000200 "cache"
+#mmcblk0p26: 3cfffe00 00000200 "userdata"
+#mmcblk0p29: 017ade00 00000200 "devlog"
+#mmcblk0p31: 00040000 00000200 "pdata"
+#mmcblk0p30: 00011c00 00000200 "extra"
+#mmcblk0p32: 05ffe000 00000200 "fat"
+#mmcblk0p27: 07fffe00 00000200 "swap"
 
 
-TARGET_USERIMAGES_USE_EXT4 := true
+#TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 585101312
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 1232072704
 BOARD_BOOTIMAGE_PARTITION_SIZE := 6194304
@@ -39,21 +45,15 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storag
 BOARD_VOLD_MAX_PARTITIONS := 36
 
 # Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/primou/bluetooth/include
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/primou/bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := device/htc/primou/bluetooth/libbt_vndcfg.txt
 
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := primou
 BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
 
-# No SDK blobs
-BUILD_EMULATOR_SENSORS_MODULE := false
-BUILD_EMULATOR_GPS_MODULE := false
-
-
 # Recovery
-BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_RECOVERY_FSTAB = device/htc/primou/rootdir/fstab.primou
+TARGET_RECOVERY_FSTAB = device/htc/msm7x30-common/rootdir/fstab.htc7x30
 RECOVERY_FSTAB_VERSION := 2
 
 # TWRP
